9fb5837a21ac931c1bef1ed134162abe557d359c
윤섭 <54024213+F-UMI@users.noreply.github.com>
2024-06-09 15:04:39 +0900

    :sparkles: feat: 물품 재고수 변경

--- src/main/java/com/univ/drip/repository/ProductRepository.java
+++ src/main/java/com/univ/drip/repository/ProductRepository.java
@@ -3,7 +3,10 @@ package com.univ.drip.repository;
 import com.univ.drip.entity.Product;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
@@ -21,4 +24,9 @@ public interface ProductRepository extends JpaRepository<Product, String> {
   Product findProductByProductId(Long productId);
 
   List<Product> findByProductCategoryAndProductCondition(String productCategory, String productCondition);
+
+  @Transactional
+  @Modifying
+  @Query("update Product p set p.quantity = ?1 where p.productId = ?2")
+  int updateQuantityByProductId(Integer quantity, Long productId);
 }
--- src/main/java/com/univ/drip/service/impl/OrderManageServiceImpl.java
+++ src/main/java/com/univ/drip/service/impl/OrderManageServiceImpl.java
@@ -6,11 +6,13 @@ import com.univ.drip.entity.CartItem;
 import com.univ.drip.entity.Member;
 import com.univ.drip.entity.Order;
 import com.univ.drip.entity.OrderItem;
+import com.univ.drip.entity.Product;
 import com.univ.drip.repository.CartItemRepository;
 import com.univ.drip.repository.CartRepository;
 import com.univ.drip.repository.MemberRepository;
 import com.univ.drip.repository.OrderItemRepository;
 import com.univ.drip.repository.OrderRepository;
+import com.univ.drip.repository.ProductRepository;
 import com.univ.drip.service.OrderManageService;
 import jakarta.servlet.http.HttpSession;
 import java.util.List;
@@ -25,15 +27,18 @@ public class OrderManageServiceImpl implements OrderManageService {
   private final CartItemRepository cartItemRepository;
   private final OrderRepository orderRepository;
   private final OrderItemRepository orderItemRepository;
+  private final ProductRepository productRepository;
 
   @Autowired
   public OrderManageServiceImpl(MemberRepository memberRepository, CartRepository cartRepository, CartItemRepository cartItemRepository,
       OrderRepository orderRepository,
-      OrderItemRepository orderItemRepository) {
+      OrderItemRepository orderItemRepository,
+      ProductRepository productRepository) {
     this.cartItemRepository = cartItemRepository;
     this.orderItemRepository = orderItemRepository;
     this.cartRepository = cartRepository;
     this.orderRepository = orderRepository;
+    this.productRepository = productRepository;
   }
 
 
@@ -46,12 +51,13 @@ public class OrderManageServiceImpl implements OrderManageService {
     List<CartItem> cartItems = cart.getCartItems();
     for (CartItem cartItem : cartItems) {
       OrderItem orderItem = OrderItem.createOrderItem(order, cartItem.getProduct(), cartItem.getCount());
+      Product product = productRepository.findById(String.valueOf(cartItem.getProduct().getProductId())).orElseThrow();
+      productRepository.updateQuantityByProductId(product.getQuantity() - cartItem.getCount(), cartItem.getProduct().getProductId());
       orderItemRepository.save(orderItem);
     }
-
+    cartRepository.updateCountByCartId(0, cart.getCartId());
     cartItemRepository.deleteByCart(cart);
     cart.getCartItems().clear();
-    cartItemRepository.updateCountByCart(0, cart);
     cartRepository.save(cart);
 
     return order;
